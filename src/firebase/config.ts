3131251c66477c09547a004329f9b7da748b76ea
Techassociatesgroups <techassociatesgroups@gmail.com>
2025-11-17 15:11:14 +0530

    Stop tracking config.ts and ignore it

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-3621455774-eef78",
-  "appId": "1:779022583775:web:d3b69d9b700769ed80666a",
-  "apiKey": "AIzaSyAfadPhzJvlM7FIJwO706RsFFDznmDNaZU",
-  "authDomain": "studio-3621455774-eef78.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "779022583775"
-};
